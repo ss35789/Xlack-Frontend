cb3d9c3e3e7932118c99835fc09ec357257205fe
ss35789 <ss35789@naver.com>
2022-07-11 04:20:09 +0900

    Building the Header Section

--- xlack/src/App.js
+++ xlack/src/App.js
@@ -1,10 +1,27 @@
 
 import './App.css';
+import Header from './components/Header';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <h1>hello!!!123</h1>
+      <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Header/>}>
+            
+          </Route>
+        </Routes>
+      </div>
+    </Router>
     </div>
   );
 }
