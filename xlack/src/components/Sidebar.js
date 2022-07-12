ea43e0a9a127bc40cca467846ea2720329126129
ss35789 <ss35789@naver.com>
2022-07-12 17:51:13 +0900

    add SidebarOption

--- xlack/src/components/Sidebar.js
+++ xlack/src/components/Sidebar.js
@@ -2,6 +2,17 @@ import React from'react';
 import styled from 'styled-components';
 import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 import CreateIcon from '@mui/icons-material/Create';
+import SidebarOption from './SidebarOption';
+import InsertCommentIcon from '@mui/icons-material/InsertComment';
+import InboxIcon from '@mui/icons-material/Inbox';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import AppsIcon from '@mui/icons-material/Apps';
+import FileCopyIcon from '@mui/icons-material/FileCopy';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AddIcon from '@mui/icons-material/Add';
 
 function Sidebar(){
     return(
@@ -17,7 +28,20 @@ function Sidebar(){
                 <CreateIcon/>
             </SidebarHeader>
 
-
+            <SidebarOption Icon={InsertCommentIcon} title='Threads'/> 
+            <SidebarOption Icon={InboxIcon} title='Mention & reactions'/> 
+            <SidebarOption Icon={DraftsIcon} title='Saved items'/> 
+            <SidebarOption Icon={BookmarkBorderIcon} title='Channel browser'/> 
+            <SidebarOption Icon={PeopleAltIcon} title='People & user groups'/> 
+            <SidebarOption Icon={AppsIcon} title='Apps'/> 
+            <SidebarOption Icon={FileCopyIcon} title='File browser'/> 
+            <SidebarOption Icon={ExpandLessIcon} title='Show less'/> 
+            <hr />
+            <SidebarOption Icon={ExpandMoreIcon} title='Channels'/>
+            <hr />
+            <SidebarOption Icon={AddIcon} addChannelOption title='Add Channel'/>
+           
+            {/* db 안의 <SidebarOption ... /> 표시 */}
             
         </SidebarContainer>
     )
--- xlack/src/components/SidebarOption.js
+++ xlack/src/components/SidebarOption.js
@@ -1,12 +1,48 @@
 import React from'react';
+import styled from'styled-components';
+function SidebarOption({Icon,title,addChannelOption}){
 
-function SidebarOption(){
+    const addChannel=()=>{
+        const channelName=prompt('Please enter the channel name')
+        // db에 name: channelName 방추가
+    }
+    const selectChannel=()=>{
+
+    }
+
     return(
-        <div>
-
-        </div>
+        <SidebarOptionContainer
+            onClick={addChannelOption ? addChannel : selectChannel}
+        >
+            {Icon && <Icon fontSize='small' style={{padding : 10}}></Icon>}
+            {Icon ? (<h3>{title}</h3>) : 
+                <SidebarOptionChannel>
+                    <span>#</span>{title}
+                </SidebarOptionChannel>
+            }
+        </SidebarOptionContainer>
     )
 }
 export default SidebarOption;
 
-const SidebarOptionContainer=styled.div``;
+const SidebarOptionContainer=styled.div`
+    display : flex;
+    font-size: 12px;
+    padding :10px;
+    align-items: center;
+    cursor: pointer;
+
+    :hover{
+        opacity: 0.9;
+        background-color:#340e36;
+    }
+   
+    >h3{
+        font-weight:500;
+    }
+
+    >h3 >span{
+        padding: 15px;
+    }
+`;
+const SidebarOptionChannel=styled.div``;
