492c7a85d59fc09fa2fc693b538b21d1f6757a28
ryankimjh00 <ryankimjh546@gmail.com>
2022-12-30 22:58:34 +0900

    add slack logo

--- src/pages/FirstPage.tsx
+++ src/pages/FirstPage.tsx
@@ -3,26 +3,30 @@ import styled, {keyframes} from 'styled-components';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import {bounceInRight} from 'react-animations';
-
 function FistPage() {
   return (
     <>
       <LoginContainer>
-        <button
-          onClick={e => {
-            e.preventDefault();
-            window.location.href = '/login';
-          }}
-        >
-          Go to Login
-        </button>
-        <LoginMessage>First Page</LoginMessage>
-        <LoginMessage>
-          Is your team new to Xlack?&nbsp;
-          <a href={'/workspace'} style={{color: 'white', fontSize: '20px'}}>
-            Create a new Workspace
-          </a>
-        </LoginMessage>
+        <Contents>
+          <img src="../image/slackLogo.png" alt={'slackLogo'}></img>
+          <Titlemessage>
+            Xlack brings the <br /> team together <br /> wherever you are
+          </Titlemessage>
+          <button
+            onClick={e => {
+              e.preventDefault();
+              window.location.href = '/login';
+            }}
+          >
+            Go to Login
+          </button>
+          <Submessage>
+            Is your team new to Xlack?&nbsp;
+            <a href={'/workspace'} style={{color: 'white', fontSize: '20px'}}>
+              Create a new Workspace
+            </a>
+          </Submessage>
+        </Contents>
       </LoginContainer>
     </>
   );
@@ -31,25 +35,11 @@ function FistPage() {
 export default FistPage;
 
 const rightShift = keyframes`${bounceInRight}`;
+
 const LoginContainer = styled.div`
   background-color: #3f0f40;
   height: 100vh;
   display: grid;
-  place-items: center;
-
-  > button {
-    margin-top: 300px;
-    align-self: center;
-    width: 500px;
-    height: 60px;
-    background-color: #457359;
-    font-size: 30px;
-    font-weight: bolder;
-    color: white;
-    border: none;
-    border-radius: 5px;
-    animation: 1s ${rightShift};
-  }
 `;
 
 const LoginImageContainer = styled.div`
@@ -68,10 +58,41 @@ const LoginImageContainer = styled.div`
   margin-bottom: -700px;
 `;
 
-const LoginMessage = styled.div`
+const Titlemessage = styled.div`
+  color: white;
+  font-size: 60px;
+  position: relative;
+  margin-top: 10px;
+  font-weight: bold;
+  margin-right: 50%;
+  animation: 1s ${rightShift};
+`;
+
+const Submessage = styled.div`
   color: white;
   font-size: 20px;
   position: relative;
   margin-top: 10px;
   animation: 1s ${rightShift};
+`;
+
+const Contents = styled.div`
+  background-color: black;
+  width: 1000px;
+  height: 400px;
+  margin-right: 40%;
+  margin-top: 10px;
+  > button {
+    margin-top: 300px;
+    align-self: center;
+    width: 500px;
+    height: 60px;
+    background-color: #457359;
+    font-size: 30px;
+    font-weight: bolder;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    animation: 1s ${rightShift};
+  }
 `;
