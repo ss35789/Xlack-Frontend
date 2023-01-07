3442265b6f4fe7e33ffc2b304a5311161dddd85d
Jonghyeon Kim <ryankimjh546@gmail.com>
2023-01-07 23:09:43 +0900

    fix font size

--- src/components/Status/StatusManual.tsx
+++ src/components/Status/StatusManual.tsx
@@ -69,6 +69,7 @@ const StatusButton = styled.button`
   background-color: aliceblue;
   border: none;
   text-align: left;
+  font-size: 20px;
   :hover {
     background-color: cornflowerblue;
   }
