297a67678bb8f9dca43c798543fff5536b747541
ryankimjh00 <ryankimjh546@gmail.com>
2023-01-08 00:54:56 +0900

    test

--- src/components/Status/StatusManual.tsx
+++ src/components/Status/StatusManual.tsx
@@ -69,7 +69,7 @@ const StatusButton = styled.button`
   background-color: aliceblue;
   border: none;
   text-align: left;
-  font-size: 14px;
+  font-size: 13px;
   :hover {
     background-color: cornflowerblue;
   }
