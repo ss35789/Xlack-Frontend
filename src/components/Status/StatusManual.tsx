b4a3193eac051882f400b663000d8e8b348667cf
ryankimjh00 <ryankimjh546@gmail.com>
2023-01-07 23:11:34 +0900

    fix font size2

--- src/components/Status/StatusManual.tsx
+++ src/components/Status/StatusManual.tsx
@@ -69,7 +69,7 @@ const StatusButton = styled.button`
   background-color: aliceblue;
   border: none;
   text-align: left;
-  font-size: 25px;
+  font-size: 20px;
   :hover {
     background-color: cornflowerblue;
   }
