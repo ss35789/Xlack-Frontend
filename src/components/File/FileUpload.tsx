cd0a56cf8a43651d8c8a4e2607e55a86ab60609a
ryankimjh00 <ryankimjh546@gmail.com>
2023-02-23 22:42:16 +0900

    배열로 저장이 됨을 확인 근데 유지가 안됨 redux?

--- src/components/File/FileUpload.tsx
+++ src/components/File/FileUpload.tsx
@@ -3,7 +3,7 @@ import "react-dropdown/style.css";
 import DialogContent from "@material-ui/core/DialogContent";
 import Dialog from "@material-ui/core/Dialog";
 import styled from "styled-components";
-import { Paper } from "@material-ui/core";
+import { Button, Paper } from "@material-ui/core";
 import { FileUploader } from "react-drag-drop-files";
 
 const FileUpload = () => {
@@ -14,8 +14,11 @@ const FileUpload = () => {
   const fileArray: unknown[] = [];
   const handleChange = (file: any) => {
     setFile(file);
-    uploadedFiles.push(file.name);
+    uploadedFiles.push(file);
     console.log(file.name + "업로드 성공");
+    console.log(uploadedFiles);
+  };
+  const clickToShow = async () => {
     console.log(uploadedFiles);
   };
   const handleClickToOpen = async () => {
@@ -35,6 +38,7 @@ const FileUpload = () => {
       <Dialog fullWidth={true} open={open} onClose={handleToClose} PaperComponent={StyledPaper}>
         <DialogContent>
           <FileUploader handleChange={handleChange} name="file" types={fileTypes} />
+          <Button onClick={clickToShow}>click to see files</Button>
           {fileArray}
         </DialogContent>
       </Dialog>
