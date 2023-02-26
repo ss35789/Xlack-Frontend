43a6a9bc872632c7b6c2fd616e452f29aa3fac42
ryankimjh00 <ryankimjh546@gmail.com>
2023-02-26 15:28:24 +0900

    status를 리덕스 컨테이너에 저장

--- src/variable/StatusSlices.ts
+++ src/variable/StatusSlices.ts
@@ -1,19 +1,21 @@
 import { Status } from "../components/types";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-
-const initialState: Status = {
-  status_message: "",
-  status_icon: "",
-  until: 0,
+interface struct {
+  statusData: Status;
+}
+const initialState: struct = {
+  statusData: {
+    status_message: "Default Status Message",
+    status_icon: "Default Status Icon",
+    until: 100,
+  },
 };
 export const StatusSlice = createSlice({
   name: "ClickedStatus",
   initialState,
   reducers: {
     setStatus: (state, action: PayloadAction<Status>) => {
-      state.status_message = action.payload.status_message;
-      state.status_icon = action.payload.status_icon;
-      state.until = action.payload.until;
+      state.statusData = action.payload;
     },
   },
 });
