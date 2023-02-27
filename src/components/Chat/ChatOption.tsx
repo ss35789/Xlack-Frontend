0f5b2846174cff1a6821c8966326d136158acf00
ss35789 <ss35789@naver.com>
2023-02-27 15:37:35 +0900

    ChatOption 수정
    chatOption의 기능추가와  DetailMessage를 마우스를 올리면 볼 수 있음 css 수정 필요

--- src/components/Chat/ChatOption.tsx
+++ src/components/Chat/ChatOption.tsx
@@ -1,10 +1,12 @@
-import { PushpinOutlined } from "@ant-design/icons";
+import { PushpinOutlined, RadarChartOutlined } from "@ant-design/icons";
 import styled from "styled-components";
 import { ChatType } from "../../types/types";
 import { at, backUrl } from "../../variable/cookie";
 import axios from "axios";
+import { useState } from "react";
 
 const ChatOption = (chat: ChatType) => {
+  const [showDetail, setShowDetail] = useState<number>(-1);
   const MakeChatBookmark = async () => {
     const cid = parseInt(chat.id);
     //chat/bookmark에 들어가는 chat_id는 다른 데이터구조(string)과는 달리 number라 형변환
@@ -27,26 +29,59 @@ const ChatOption = (chat: ChatType) => {
         console.log(err);
       });
   };
+
+  const ChatOptionDetailArray = [
+    {
+      detailMessage: "ChatBookmark",
+      func: () => {
+        MakeChatBookmark();
+      },
+      Icon: <PushpinOutlined />,
+    },
+    {
+      detailMessage: "test",
+      func: () => {
+        console.log("test");
+      },
+      Icon: <RadarChartOutlined />,
+    },
+  ];
   return (
     <>
-      chatOPtions
-      <Icon
-        onClick={() => {
-          MakeChatBookmark();
-        }}
-      >
-        <PushpinOutlined />
-      </Icon>
+      {ChatOptionDetailArray &&
+        ChatOptionDetailArray.map((ChatOptionDetail, i) => {
+          return (
+            <Option
+              key={i}
+              onClick={() => {
+                ChatOptionDetail.func();
+              }}
+              onMouseOver={() => {
+                setShowDetail(i);
+              }}
+              onMouseLeave={() => {
+                setShowDetail(-1);
+              }}
+            >
+              {showDetail === i && <ChatOptionDetailMessage de={ChatOptionDetail.detailMessage} />}
+              {ChatOptionDetail.Icon}
+            </Option>
+          );
+        })}
     </>
   );
 };
 
 export default ChatOption;
 
-const Icon = styled.span`
+const Option = styled.span`
   :hover {
     cursor: pointer;
     opacity: 0.6;
     background-color: #9ca3af;
   }
 `;
+
+const ChatOptionDetailMessage = (props: any) => {
+  return <>{props.de}</>;
+};
