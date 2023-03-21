9489507071d25f7ae183c36d62c84bbc07c13b40
ryankimjh00 <ryankimjh546@gmail.com>
2023-03-21 14:08:37 +0900

    make fileslice

---
+++ src/variable/ChatSlice.ts
@@ -0,0 +1,26 @@
+import { FileType } from "../types/types";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+interface struct {
+  SendMessage: {
+    message: string;
+    fileData: File | null;
+  };
+}
+const initialState: struct = {
+  SendMessage: {
+    message: "...",
+    fileData: null,
+  },
+};
+export const ChatSlice = createSlice({
+  name: "Chat",
+  initialState,
+  reducers: {
+    setFile: (state, action: PayloadAction<File>) => {
+      state.SendMessage.fileData = action.payload;
+    },
+  },
+});
+
+export const { setFile } = ChatSlice.actions;
+export default ChatSlice.reducer;
