d300ac8ecbe357fdc9aaf3a2b8f3f9116362ff05
ryankimjh00 <ryankimjh546@gmail.com>
2023-03-21 17:48:58 +0900

    revert ChatSlice.ts

---
+++ src/variable/ChatSlice.ts
@@ -0,0 +1,25 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+interface struct {
+  SendMessage: {
+    message: string;
+    fileData: File | null;
+  };
+}
+const initialState: struct = {
+  SendMessage: {
+    message: File.toString(),
+    fileData: null,
+  },
+};
+export const ChatSlice = createSlice({
+  name: "Chat",
+  initialState,
+  reducers: {
+    setFile: (state, action: PayloadAction<File>) => {
+      state.SendMessage.fileData = action.payload;
+    },
+  },
+});
+
+export const { setFile } = ChatSlice.actions;
+export default ChatSlice.reducer;
