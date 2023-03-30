ccc6cdb6b595d26583808d9c2fd57a1e0c29ddf7
ss35789 <ss35789@naver.com>
2023-03-30 18:13:45 +0900

    채널 설명 편집 기능 구현도중

--- src/components/Channel/ChannelSetting_detail/About.tsx
+++ src/components/Channel/ChannelSetting_detail/About.tsx
@@ -1,5 +1,31 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../../../app/store";
+import axios from "axios";
+import { at, backUrl } from "../../../variable/cookie";
+
 const About = () => {
-  return <h1>about something!</h1>;
+  const rightClickedChannelData = useSelector((state: RootState) => state.getMyWorkSpace.SearchedChannel);
+  const currentWorkspace = useSelector((state: RootState) => state.getMyWorkSpace);
+  const editChannnelDesc = async () => {
+    await axios
+      .patch(
+        `${backUrl}channel/${currentWorkspace.ClickedWorkSpace.hashed_value}/`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${at}`,
+          },
+        },
+      )
+      .catch(err => console.log(err));
+  };
+  return (
+    <>
+      <h1>about this channel!</h1>
+      <hr />
+      <h1>${rightClickedChannelData.description}</h1>
+    </>
+  );
 };
 
 export default About;
